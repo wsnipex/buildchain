898020f02eb9de91e70f99182aed07a43c7818ed
howie-f <rftc@gmx.de>
2022-05-14 19:52:17 +0200

    switch to api android-30 / build tools 30.0.2

--- variables.sh
+++ variables.sh
@@ -13,8 +13,8 @@ IDE=codium
 # android
 SDKZIP=https://dl.google.com/android/repository/commandlinetools-linux-8092744_latest.zip
 NDKZIP=https://dl.google.com/android/repository/android-ndk-r21e-linux-x86_64.zip
-PLATFORMS="platforms;android-28"
-BUILDTOOLS="build-tools;28.0.3"
+PLATFORMS="platforms;android-30"
+BUILDTOOLS="build-tools;30.0.2"
 
 # set this directory where you cloned LibreElec repo to
 LEREPO=~/repos/le
